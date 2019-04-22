7f6b46a2df45af1da33c53248f1b0c7d0ee48aca
zhouyangyong <3108255033@qq.com>
2019-04-22 19:15:10 +0800

    旅梦学习

--- css/landRover-B/js/main.js
+++ css/landRover-B/js/main.js
@@ -8,4 +8,13 @@
             icon.text('search').parents('.content').removeClass('searching');
         }
     })
+    // carousel
+    // $('.ui.hero')
+    // semantic.js
+    $('.ui.sidebar')
+        .sidebar('setting', 'dimPage', true)
+        .sidebar('attach events', '.ui.navbar .menu-icon', 'show')
+        .sidebar('attach events', '.ui.sidebar close-icon', 'hidden')
+    // sidebar
+    $('.ui.navbar .menu').clone().appendTo('.ui.sidebar');
 })();
