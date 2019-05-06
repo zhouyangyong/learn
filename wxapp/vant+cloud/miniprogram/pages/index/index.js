51fcfc6d9b10ae5a12ff8b97e411015e5eec8623
zhouyangyong <3108255033@qq.com>
2019-05-06 21:39:09 +0800

    旅梦学习

---
+++ wxapp/vant+cloud/miniprogram/pages/index/index.js
@@ -0,0 +1,98 @@
+// miniprogram/pages/index/index.js
+import Notify from '../dist/notify/notify';
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    newGroupModel: false,
+    groupName: ''
+  },
+  showNewGroupModel() {
+    this.setData({
+      newGroupModel: true
+    })
+  },
+  onGroupNameChange(event) {
+    this.setData({
+      groupName: event.detail
+    })
+    console.log(event.detail)
+  },
+  closeDiolog() {
+    this.setData({
+      newGroupModel: false
+    })
+  },
+  creatGroup() {
+    let self = this;
+    if(self.data.groupName === '') {
+      Notify({
+        text: '起个名字吧',
+        duration: 1500,
+        selector: '#notify-selector',
+        backgroundColor: '#dc3545'
+      });
+      self.selectComponent('#new-group-model').stopLoading();
+      return
+    } else {
+      
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
