04eb6542f372a350555cb7421c63ed14cfe427fd
zhouyangyong <3108255033@qq.com>
2019-05-18 14:18:10 +0800

    webpack学习

---
+++ js/webpack/test/webpack.config.js
@@ -0,0 +1,29 @@
+// 打包配置流程
+// 如果没有这个文件, 默认配置
+// src/ 开发目录
+// dist/
+// src/index.js
+const path = require('path');
+module.exports = {
+    entry: './src/index',
+    output: {
+        path: path.resolve(__dirname),
+        filename: '[name].js'
+    },
+    module: {
+        // babel
+        rules: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, 'src')
+                ],
+                use: 'babel-loader'
+            }
+        ]
+        // stylus, 静态资源
+    },
+    resolve: {
+        extensions: ['.js'],
+    }
+}
