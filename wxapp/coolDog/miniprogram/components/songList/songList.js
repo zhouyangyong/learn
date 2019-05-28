8faeea55fb4da64ba106c49d4b92d37f1386a8f7
zhouyangyong <3108255033@qq.com>
2019-05-28 19:17:09 +0800

    coolDog

--- wxapp/coolDog/miniprogram/components/songList/songList.js
+++ wxapp/coolDog/miniprogram/components/songList/songList.js
@@ -11,13 +11,17 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    show: false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onPopup () {
+      this.setData({
+        show: true
+      })
+    }
   }
 })
