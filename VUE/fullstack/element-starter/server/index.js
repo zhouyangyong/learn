78666e02a32218b2eeccc21d866050ce1b529880
zhouyangyong <3108255033@qq.com>
2019-06-18 14:34:16 +0800

    fullstack跨域

---
+++ VUE/fullstack/element-starter/server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const http = require('http');
+const app = express(); // 应用
+// /api 祸端api 服务
+const api = require('./api');
+
+app.use(api);
+
+app.set('port', 3000);
+
+http
+  .createServer(app)
+  .listen(app.get('port'), '0.0.0.0', function () {
+    console.log('Express server listening on port');
+  })
+
