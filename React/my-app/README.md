6102701b0d530b4beaee75ce423f1d41372d360f
zhouyangyong <3108255033@qq.com>
2019-07-02 17:17:07 +0800

    ts

---
+++ React/my-app/README.md
@@ -0,0 +1,9 @@
+- vue-cli 3.0+  原生命令行支持 typescript 
+  react 最早拥抱，vue 加入 
+
+- 项目多加20% typescript代码 本来不用写的 
+  少了60%的潜在bug(js 弱类型) 良好的代码提示 让团队合作体验更好
+
+- 重要的对象（组件，state，prop，vuex/redux，函数），声明类型
+  vuex最大的对象 state
+  interface
---
+++ React/my-app/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Action } from 'vuex-class';
+export default class Home extends Vue {
+  private cate: string = 'in_theaters';
+  private active: number = 0;
+  private loading: boolean = true;
+  private movieListData = null;
+  @Action private movieList!: (cate: string) => any;
+
+  private created() {
+    this.movieList(this.cate)
+      .then((res: any) => {
+        this.movieListData = res.subjects;
+        this.loading = false;
+        console.log(res.subjects);
+      })
+  }
+}
+</script>
