d55e6cb77b26c6038bd8df0bfa8312b19661922b
zhouyangyong <3108255033@qq.com>
2019-07-20 14:37:49 +0800

    koa-source

---
+++ interview/ES-module/main.js
@@ -0,0 +1,10 @@
+export default {
+  a: 1,
+  b: 2
+}
+
+// default export
+// name export
+
+export const a = 3;
+export const b = 4;
---
+++ node/koa-source/onion.js
@@ -0,0 +1,14 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+  console.log(1);
+  await next();
+  console.log(2);
+})
+app.use(async (ctx) => {
+  ctx.body = '1230';
+  console.log(3);
+})
+
+app.listen(8899);
