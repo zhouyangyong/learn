6a0d41603398dc7d9a25d28980ae0d9ab420164e
zhouyangyong <3108255033@qq.com>
2019-07-25 21:08:45 +0800

    trip

---
+++ VUE/trip/src/components/RouterError/404.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="md-example-child md-example-child-result-page md-example-child-result-page-0">
+    <md-result-page
+      type="lost">
+    </md-result-page>
+  </div>
+</template>
+
+<script>
+import {ResultPage} from 'mand-mobile'
+
+export default {
+  name: 'result-page-demo',
+  /* DELETE */
+  title: '404',
+  /* DELETE */
+  components: {
+    [ResultPage.name]: ResultPage,
+  },
+}
+
+</script>
+
+<style lang="stylus">
+.md-example-child-result-page-0
+  background #FFF
+</style>
