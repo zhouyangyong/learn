04d64e62472881bddc0f5c5fe7a99cc799ed116b
zhouyangyong <3108255033@qq.com>
2019-08-02 22:06:38 +0800

    typescript

---
+++ interview/typescript/type/main.js
@@ -0,0 +1,15 @@
+// // console.log('hello ts');
+// let decLiteral: number = 1;
+// decLiteral = 1212;
+// let hexLiteral: number = 0xf00d;
+// let user_name: string = 'bob';
+// let sentence: string = `Hello, my name is ${name}`;
+// let list: number[] = [1, 2, 3] 
+// let list2: Array<number> = [1, 2, 3]
+// function div(x) {
+//   // 1000 / x
+//   if(isFinite(1000 / x)) {
+//     return 'Number is NOT Infinity'
+//   }
+// }
+// console.log(div(0));
